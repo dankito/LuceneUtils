b74ffb472eba8bc3ee53cef3453087a5f5b503e5
dankl <dankito@users.noreply.github.com>
2020-04-11 01:24:41 +0200

    Added io.codearte.nexus-staging plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,12 @@ buildscript {
 }
 
 
+plugins {
+        // So after executing publish staged repository can be closed and released by executing closeAndReleaseRepository
+        id("io.codearte.nexus-staging") version "0.21.2"
+}
+
+
 val commonScriptsFile = File(File(project.gradle.gradleUserHomeDir, "scripts"), "commonScripts.gradle")
 if (commonScriptsFile.exists()) {
     apply(from = commonScriptsFile)
